8d8b9bdb0d5ece5fe47d508d33f6c8e4c1fe4e33
pranu-mk <pranalikale14574@gmail.com>
2026-02-11 19:42:59 +0530

    enable railway ssl connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Create a connection pool (efficient way to handle multiple users)
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
