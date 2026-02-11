a54edf1e0e596061d1c50f27a0998b1a45e81861
pranu-mk <pranalikale14574@gmail.com>
2026-02-11 19:58:45 +0530

    use railway database url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-         minVersion: 'TLSv1.2',
-        rejectUnauthorized: false
-    }
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
-
-// Export the promise-based version so we can use async/await
 module.exports = pool.promise();
