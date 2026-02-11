eeb58d4fdc2d773689b97f1886463e75cb36adc8
pranu-mk <pranalikale14574@gmail.com>
2026-02-11 23:06:18 +0530

    NA

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
+
 
 module.exports = pool.promise();
