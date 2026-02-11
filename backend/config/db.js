b058dfbdf905fdb522f5574123339c74e4859c85
pranu-mk <pranalikale14574@gmail.com>
2026-02-11 19:15:59 +0530

    added db port for railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,10 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-    
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
+
 
 // Export the promise-based version so we can use async/await
 module.exports = pool.promise();
